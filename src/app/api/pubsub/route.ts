45b3d81b833304a1e15fc2c2884c6b0ff3fefe0f
umarkhan110 <umardraz277@gmail.com>
2025-02-04 11:57:53 +0500

    50

--- src/app/api/pubsub/route.ts
+++ src/app/api/pubsub/route.ts
@@ -53,7 +53,7 @@ export async function POST(request: Request) {
         // ✅ Fetch Gmail History Changes
         const history = await gmail.users.history.list({
             userId: "me",
-            startHistoryId: String(historyId - 10),
+            startHistoryId: String(historyId - 50),
         });
 
         // console.log("📨 Gmail History Changes:", history.data);
